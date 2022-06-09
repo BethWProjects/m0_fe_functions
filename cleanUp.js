e1f7472ebb8ec8c200eaa87d43c306a288cbb0f0
Beth Wilson <bethwilson777@gmail.com>
2022-06-09 17:56:30 -0600

    Adding functions

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,46 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
+// I added missing semicolons at the end of the console.log() statement and at the end of the calling function
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 console.log(sum);
-      }
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// I added a semicolon after the variable and moved the end curly bracket to the left.
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
-
+// The function keyword was not completed, and I moved the las curly bracket to the correct position to close the defining function.
 
 //  EX 4:
-function average(num1, num2) 
-  {
+function average(num1, num2) {
 var sum = num1 + num2;
     var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
+  console.log(`the average is: ` + (avg));
   }
+  average(1, 2);
+// I moved the first curly bracket up and to the right of the parenthesis.  To get the average I removed the $, because we were using the avg calculation to find the answer to 'the average is'.  The function was defined but not called, so I added the function call syntax
--- functions.js
+++ functions.js
@@ -1,13 +1,38 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
-
+function printGreeting() {
+  console.log("Hello, my name is Beth.");
+}
+printGreeting();
+printGreeting();
+printGreeting();
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
-
+function dogName(name) {
+  console.log(`Hello, my dog's name is ${name}`);
+}
+dogName("Jimmy");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
+function company(name) {
+var sum = 110000 - 90000;
+  console.log(`The company: ${name} has an average pay range of: ` + (sum));
+  }
+  company("Turing");
 
 // 4: Write a function that satifies the following interaction pattern:
+function checkStock(number, item) {
+  var inStock = [4, 3, 2, 1];
+  for (var i = 0; i < inStock.length; i++){
+    if (inStock[i] === number && inStock[i] >= 4){
+      console.log(`${item} is in stock`);
+    } else if (inStock[i] === number && number <= 3 ){
+      console.log(`${item} - running LOW`);
+  } else if (number === 0){
+      console.log(`${item} - OUT of Stock`);
+      break;
+    }
+  }
+}
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
